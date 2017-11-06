4544a51966c7609672241abfb901c6fb1039b6af
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-06 21:31:28 +0000

    Make the delete button test pass

--- src/components/ItemsList/index.js
+++ src/components/ItemsList/index.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import './styles.css';
 
-export const ItemsList = ({ items }) => {
+export const ItemsList = ({ items, onDelete }) => {
   return (
     <div>
       <ul className="itemsList-ul">
         {items.length < 1 && <p id="items-missing">Add some tasks above.</p>}
-        {items.map(item => <li key={item.id}>{item.content}</li>)}
+        {items.map(item => <li key={item.id}>{item.content} <button onClick={() => {onDelete(item.id)}} /></li>)}
       </ul>
     </div>
   );
