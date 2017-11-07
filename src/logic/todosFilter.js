a25d9b224bdf0101d9c7f58ffecd55468aaaf86a
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-07 21:13:09 +0000

    Add the outline of the reducer

--- src/logic/todosFilter.js
+++ src/logic/todosFilter.js
@@ -1,1 +1,12 @@
-export default () => {}
+export const SHOW_ALL = 'qgo/assessment/SHOW_ALL';
+
+export const initialState = SHOW_ALL;
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
