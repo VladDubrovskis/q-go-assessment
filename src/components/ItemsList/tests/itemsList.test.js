3a5e8b024917a69c5977d49fd4bc640805d1f90f
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-07 20:22:25 +0000

    Add a test to make sure complete items will render with checkbox ticked and a stike through

--- src/components/ItemsList/tests/itemsList.test.js
+++ src/components/ItemsList/tests/itemsList.test.js
@@ -32,6 +32,20 @@ describe('ItemsList', () => {
     expect(renderedItem.find('li').at(1).find('input[type="checkbox"]')).toHaveLength(1);
   });
 
+  it('should render complete items with strike through and checkbox ticked', () => {
+    const items = [{ id: 1, content: 'Test 1', complete: false }, { id: 2, content: 'Test 2', complete: true }];
+    const renderedItem = shallow(<ItemsList {...defaultProps} items={items} />);
+    expect(renderedItem.find('li')).toHaveLength(2);
+
+    expect(renderedItem.find('li').at(0).find('button')).toHaveLength(1);
+    expect(renderedItem.find('li').at(0).find('s')).toHaveLength(0);
+    expect(renderedItem.find('li').at(0).find('input[type="checkbox"]').props().checked).toEqual(false);
+
+    expect(renderedItem.find('li').at(1).find('button')).toHaveLength(1);
+    expect(renderedItem.find('li').at(1).find('s')).toHaveLength(1);
+    expect(renderedItem.find('li').at(1).find('input[type="checkbox"]').props().checked).toEqual(true);
+  });
+
   it('should call onDelete with the according item id when delete button is clicked', () => {
       const onDeleteMock = jest.fn();
       const items = [{ id: 1, content: 'Test 1' }, { id: 2, content: 'Test 2' }];
