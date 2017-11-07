3ddd6c9ea69122b0020cf3d5972bed2f9b295425
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-07 21:07:46 +0000

    Add the default test for the todos filter reducer

--- src/logic/tests/todosFilter.test.js
+++ src/logic/tests/todosFilter.test.js
@@ -1,3 +1,9 @@
-/**
- * Created by vlad on 07/11/2017.
- */
+import reducer, { initialState } from '../todosFilter';
+
+describe('todos filter reducer', () => {
+  it('should use initial state if state not provided', () => {
+    const mockAction = { type: 'mystery-meat' };
+    const result = reducer(undefined, mockAction);
+    expect(result).toEqual(initialState);
+  });
+});
