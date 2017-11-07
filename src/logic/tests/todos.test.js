13b3499a08c2b496ff82aae2aade7b862237e2de
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-07 19:43:40 +0000

    Add a test for TOGGLE_ITEM action

--- src/logic/tests/todos.test.js
+++ src/logic/tests/todos.test.js
@@ -1,4 +1,4 @@
-import reducer, { initialState, addItem, deleteItem } from '../todos';
+import reducer, { initialState, addItem, deleteItem, toggleItem } from '../todos';
 
 describe('reducer', () => {
   it('should return state for unknown action', () => {
@@ -41,6 +41,20 @@ describe('reducer', () => {
     expect(result.items).toHaveLength(1);
     expect(result.items[0].id).toEqual(2);
     expect(result.items[0].content).toEqual('second');
+  });
+
+  it('should toggle item complete flag on TOGGLE_ITEM', () => {
+    const state = {
+      items: [
+        { id: 1, content: 'first', complete: false },
+        { id: 2, content: 'second', complete: true },
+      ]
+    }
+    const mockAction = toggleItem(2);
+    const result = reducer(state, mockAction);
+    expect(result.items).toHaveLength(2);
+    expect(result.items[1].id).toEqual(2);
+    expect(result.items[1].complete).toEqual(false);
   });
 
 });
