b8624ee3381153cf2c4fdfd12b6338c93f15bb9e
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-08 19:56:59 +0000

    Change the default filter state

--- src/logic/todosFilterState.js
+++ src/logic/todosFilterState.js
@@ -1,11 +1,10 @@
-export const SHOW_ALL = 'qgo/assessment/SHOW_ALL';
 export const TOGGLE_FILTER = 'qgo/assessment/TOGGLE_FILTER';
 
 export const toggleFilter = () => {
   return { type: TOGGLE_FILTER };
 };
 
-export const initialState = SHOW_ALL;
+export const initialState = false;
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
