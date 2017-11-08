049e81feaca43b35ef1428031b91463f6b964df4
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-08 20:09:03 +0000

    Add a new test to expect the Filter to have button that calls to filter toggle

--- src/components/Filter/tests/filter.test.js
+++ src/components/Filter/tests/filter.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Filter from '../index';
+
+const defaultProps = {
+  onFilterToggle: f => f,
+};
 
 describe('Filter', () => {
   it('renders without crashing', () => {
-    shallow(<Filter />);
+    shallow(<Filter {...defaultProps} />);
+  });
+
+  it('should call onFilterToggle when button is clicked', () => {
+    const onFilterToggleMock = jest.fn();
+    const renderedItem = mount(
+      <Filter {...defaultProps} onFilterToggle={onFilterToggleMock} />
+    );
+    renderedItem.find('button').simulate('click');
+    expect(onFilterToggleMock.mock.calls.length).toBe(1);
   });
 });
