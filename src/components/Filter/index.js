22ba1ffb9217c8c5faf3289cc80a730af58b1dca
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-08 20:03:33 +0000

    Make Filter tests pass

---
+++ src/components/Filter/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const Filter = () => {
+  return (
+    <div className="filter-toggle">
+      <p>Hello world</p>
+    </div>
+  );
+};
+
+export default Filter;
