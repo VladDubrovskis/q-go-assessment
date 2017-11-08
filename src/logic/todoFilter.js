61402ff10bee344fe112623eb016b7b37b18f9c1
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-08 19:59:07 +0000

    Update the filter module to not check type of filter and toggle between true and false

--- src/logic/todoFilter.js
+++ src/logic/todoFilter.js
@@ -1,7 +1,5 @@
-export const FILTER_SHOW_PENDING = 'qgo/assessment/FILTER_SHOW_PENDING';
-
 export default (items, filter) => {
-  if(filter === FILTER_SHOW_PENDING) {
+  if(filter) {
     return items.filter((item) => item.complete === false);
   }
   return items;
