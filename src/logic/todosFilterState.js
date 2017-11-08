89c4f851af912e4101b03711aeb6f2fa945a8092
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-08 19:55:29 +0000

    Update the todosFilterState module to pass the tests

--- src/logic/todosFilterState.js
+++ src/logic/todosFilterState.js
@@ -1,16 +1,16 @@
 export const SHOW_ALL = 'qgo/assessment/SHOW_ALL';
-export const SET_FILTER = 'qgo/assessment/SET_FILTER';
+export const TOGGLE_FILTER = 'qgo/assessment/TOGGLE_FILTER';
 
-export const setFilter = value => {
-  return { type: SET_FILTER, value };
+export const toggleFilter = () => {
+  return { type: TOGGLE_FILTER };
 };
 
 export const initialState = SHOW_ALL;
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case SET_FILTER:
-      return state = action.value;
+    case TOGGLE_FILTER:
+      return !state;
     default:
       return state;
   }
