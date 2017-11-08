7e649ef6a6d623dce36b108d25afe2405a519742
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-11-08 18:49:33 +0000

    Add a test for SET_FILTER

--- src/logic/tests/todosFilter.test.js
+++ src/logic/tests/todosFilter.test.js
@@ -1,9 +1,17 @@
-import reducer, { initialState } from '../todosFilter';
+import reducer, { initialState, setFilter } from '../todosFilter';
 
 describe('todos filter reducer', () => {
   it('should use initial state if state not provided', () => {
     const mockAction = { type: 'mystery-meat' };
     const result = reducer(undefined, mockAction);
     expect(result).toEqual(initialState);
+  });
+
+  it('should use initial state if state not provided', () => {
+    const state = 'DEFAULT';
+    const filterName = 'DONE';
+    const mockAction = setFilter(filterName);
+    const result = reducer(state, mockAction);
+    expect(result).toEqual(filterName);
   });
 });
